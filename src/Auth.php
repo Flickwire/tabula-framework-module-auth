a35c9c30c23bb2614e060b07bde9f1e82de08757
Skye <skye@domos.me.uk>
2019-07-15 11:53:40 +0100

    need a db handle for upgrade function

--- src/Auth.php
+++ src/Auth.php
@@ -6,6 +6,7 @@ use Tabula\Tabula;
 use Tabula\Module;
 use Tabula\Router;
 use Tabula\Router\Route;
+use Tabula\Database\AbstractAdapter;
 
 class Auth implements Module {
     private $user;
@@ -13,12 +14,10 @@ class Auth implements Module {
     //Can use this to check if user is logged in
     public $isLoggedIn;
 
-    public function upgrade(string $version): string{
+    public function upgrade(string $version, AbstractAdapter $db): string{
         //Initial Setup
         if ($version === ''){
             //Set up db tables
-            $db = $tabula->db;
-
             $db->query('CREATE TABLE tb_users (id BIGINT AUTO_INCREMENT NOT NULL, email NVARCHAR(320) NOT NULL, passwd VARCHAR(255) NOT NULL, displayname NVARCHAR(200) NOT NULL, CONSTRAINT PK PRIMARY KEY (id));');
             $db->query('CREATE TABLE tb_usergroups (id BIGINT AUTO_INCREMENT NOT NULL, displayname NVARCHAR(255) NOT NULL, CONSTRAINT PK PRIMARY KEY (id));');
             $db->query('CREATE TABLE tb_users_usergroups (user BIGINT NOT NULL, usergroup BIGINT NOT NULL, CONSTRAINT PK PRIMARY KEY (user,usergroup), CONSTRAINT FK_user FOREIGN KEY (user) REFERENCES tb_users(id), CONSTRAINT FK_usergroup FOREIGN KEY (usergroup) REFERENCES tb_usergroups(id));');
